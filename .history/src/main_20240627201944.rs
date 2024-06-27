82c96a4462a1e152a1dac4b100787b12d20be7aa
tushcmd <muturidavid854@gmail.com>
2024-06-27 20:20:57 +0300

    println!()

--- .history/src/main_20240627201944.rs
+++ .history/src/main_20240627201944.rs
@@ -3,6 +3,6 @@ fn main() {
     println!(); // prints just a newline
     println!("hello there!");
     println!("format {} arguments", "some");
-    let local_variable = "many";
+    let local_variable = "some";
     println!("format {local_variable} arguments");
 }
