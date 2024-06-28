d88d527f04efc12fb6d836e35fea098a66655f6e
tushcmd <muturidavid854@gmail.com>
2024-06-29 02:06:28 +0300

    md

---
+++ .history/src/main_20240628162801.rs
@@ -0,0 +1,16 @@
+// use regex::Regex;
+
+// fn main() {
+//     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+//     println!("Did our date match? {}", re.is_match("2014-01-01"));
+// }
+/*
+fn main() {
+    println!("Hello, world!");
+    println!(); // prints just a newline
+    println!("hello there!");
+    println!("format {} arguments", "some");
+    let local_variable = "many";
+    println!("format {local_variable} arguments");
+}
+*/
